300eb72b0bff3a682867a15948c273c316f7a769
tonipenava <85849246+tonipenava@users.noreply.github.com>
2024-04-10 17:44:26 +0200

    removing text overlaying video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,19 +19,6 @@ const Home = () => {
       >
         <source src={video} type="video/mp4" />
       </video>
-      <div
-        style={{
-          transform: isInView ? 'none' : 'translate(-200px)',
-          opacity: isInView ? 1 : 0,
-          transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.7s',
-        }}
-        className="text-center w-4/5 rounded-xl max-w-screen-sm absolute flex justify-center items-center flex-col p-3 backdrop-blur-lg "
-      >
-        <h1 className=" text-6xl text-center uppercase border-b-4 border-zinc-50 p-5 font-bold ">
-          Dolac grad
-        </h1>
-        <h2 className="text-center text-2xl">Pronađite savršen dom za sebe</h2>
-      </div>
     </section>
   );
 };
